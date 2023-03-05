cea5ef25ea10d2ae7162e43d37019d24f5b4dcaf
West <36251578+weezle1@users.noreply.github.com>
2023-03-05 14:32:33 -0500

    Java Data Types added to public  class Test

--- JavaTestProject/src/Test.java
+++ JavaTestProject/src/Test.java
@@ -1,4 +1,24 @@
 
 public class Test {
-
+	
+	public static void main(String[] args) {
+		/*** data types
+		 * 
+		String - stores text, such as "Hello". String values are surrounded by double quotes
+		int - stores integers (whole numbers), without decimals, such as 123 or -123
+		float - stores floating point numbers, with decimals, such as 19.99 or -19.99
+		char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+		boolean - stores values with two states: true or false
+		*/
+		
+		int myNum = 5;               // Integer (whole number)
+		float myFloatNum = 5.99f;    // Floating point number
+		char myLetter = 'D';         // Character
+		boolean myBool = true;       // Boolean
+		String myText = "Hello";     // String
+		
+		System.out.println("myNum=" + myNum);
+		System.out.println("myFloatNum=" + myFloatNum);
+	}
+	
 }
